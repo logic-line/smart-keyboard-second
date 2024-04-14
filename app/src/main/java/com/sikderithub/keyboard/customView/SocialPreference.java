089f119e1381331aeb307fc4c01eb679a5299f48
Partha Roy <partharoypc@users.noreply.github.com>
2024-04-14 16:14:10 +0600

    Social Info Added

---
+++ app/src/main/java/com/sikderithub/keyboard/customView/SocialPreference.java
@@ -0,0 +1,56 @@
+package com.sikderithub.keyboard.customView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.sikderithub.keyboard.R;
+
+public class SocialPreference extends Preference {
+
+    public SocialPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.custom_preference_social);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        LottieAnimationView btnFacebook = (LottieAnimationView) holder.findViewById(R.id.btnFacebook);
+        LottieAnimationView btnYoutube = (LottieAnimationView) holder.findViewById(R.id.btnYoutube);
+        LottieAnimationView btnWeb = (LottieAnimationView) holder.findViewById(R.id.btnWebIcon);
+
+        btnFacebook.setOnClickListener(v -> customMethod("btnFacebook"));
+        btnYoutube.setOnClickListener(v -> customMethod("btnYoutube"));
+        btnWeb.setOnClickListener(v -> customMethod("btnWeb"));
+    }
+
+    private void customMethod(String buttonId) {
+        switch (buttonId) {
+            case "btnFacebook":
+                openURL("https://www.facebook.com");
+                break;
+            case "btnWeb":
+                openURL("https://sikderithub.com");
+                break;
+            case "btnYoutube":
+                openURL("https://www.youtube.com");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void openURL(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        getContext().startActivity(intent);
+    }
+}
